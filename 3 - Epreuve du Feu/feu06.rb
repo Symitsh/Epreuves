367fc9eecd3402c5442ae660fafe654c718e6694
florent sandri <sandri822@hotmail.fr>
2022-12-17 16:47:10 +0100

    🔥 Add "Epreuve du Feu"

--- 3 - Epreuve du Feu/feu06.rb
+++ 3 - Epreuve du Feu/feu06.rb
@@ -1,4 +1,4 @@
 # ***** Feu : ok *****
 # Créez un programme qui célèbre votre victoire.
 
-puts "J’ai terminé l’Épreuve du Feu et c’était le exellent !"
+puts "J’ai terminé l’Épreuve du Feu et c’était excellent !"
